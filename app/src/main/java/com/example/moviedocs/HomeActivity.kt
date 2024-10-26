5bb608371cb402296909bc3160426c8c6cf823b6
MeansKane <phamminhkhangpy2003@gmail.com>
2024-10-27 00:20:06 +0700

    Update project

--- app/src/main/java/com/example/moviedocs/HomeActivity.kt
+++ app/src/main/java/com/example/moviedocs/HomeActivity.kt
@@ -2,10 +2,12 @@ package com.example.moviedocs
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
+    installSplashScreen()
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    setContentView(R.layout.activity_home)
   }
 }
