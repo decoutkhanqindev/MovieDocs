b21e9629f1298715431b6efca981af2c5ac06025
MeansKane <phamminhkhangpy2003@gmail.com>
2024-10-29 00:16:29 +0700

    Update project

--- app/src/main/java/com/example/moviedocs/MainActivity.kt
+++ app/src/main/java/com/example/moviedocs/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.moviedocs
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.moviedocs.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+  private val binding: ActivityMainBinding by lazy(LazyThreadSafetyMode.NONE) {
+    ActivityMainBinding.inflate(layoutInflater)
+  }
+  
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_home)
+    setContentView(binding.root)
   }
 }
