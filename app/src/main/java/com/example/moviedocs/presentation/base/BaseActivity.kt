9eb69955ab5fa73d75b7450eb7d9380c0a6bcc77
MeansKane <phamminhkhangpy2003@gmail.com>
2024-10-31 01:16:53 +0700

    Add BaseActivity with generic ViewBinding for all Activity in the project

---
+++ app/src/main/java/com/example/moviedocs/presentation/base/BaseActivity.kt
@@ -0,0 +1,18 @@
+package com.example.moviedocs.presentation.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseActivity<VB: ViewBinding> (
+  private val inflate: (LayoutInflater) -> VB
+): AppCompatActivity() {
+  protected lateinit var binding: VB
+  
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    binding = inflate(layoutInflater)
+    setContentView(binding.root)
+  }
+}
