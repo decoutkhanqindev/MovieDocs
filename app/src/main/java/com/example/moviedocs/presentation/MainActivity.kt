858f3703988bb5e4791a6c2b7dfb241ba88b9de7
MeansKane <phamminhkhangpy2003@gmail.com>
2024-10-30 23:36:02 +0700

    Update project

--- app/src/main/java/com/example/moviedocs/presentation/MainActivity.kt
+++ app/src/main/java/com/example/moviedocs/presentation/MainActivity.kt
@@ -1,10 +1,7 @@
-package com.example.moviedocs
+package com.example.moviedocs.presentation
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.moviedocs.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
