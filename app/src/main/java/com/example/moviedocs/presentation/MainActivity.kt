3a120d080dc06edc885d3cc16f64caeb38bbac9e
MeansKane <phamminhkhangpy2003@gmail.com>
2024-11-01 21:12:43 +0700

    Handle navigate from MainActivity to SignInFragment

--- app/src/main/java/com/example/moviedocs/presentation/MainActivity.kt
+++ app/src/main/java/com/example/moviedocs/presentation/MainActivity.kt
@@ -1,14 +1,8 @@
 package com.example.moviedocs.presentation
 
-import android.os.Bundle
 import com.example.moviedocs.databinding.ActivityMainBinding
 import com.example.moviedocs.presentation.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
-  
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-  }
-}
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate)
