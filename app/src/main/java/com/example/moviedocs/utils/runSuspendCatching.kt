a7eb24f4244bfd83bb136a667efcd269e56003e9
MeansKane <phamminhkhangpy2003@gmail.com>
2024-11-03 00:27:55 +0700

    Add runSuspendCatching utility function for executing suspending functions with error handling

---
+++ app/src/main/java/com/example/moviedocs/utils/runSuspendCatching.kt
@@ -0,0 +1,32 @@
+package com.example.moviedocs.utils
+
+import kotlinx.coroutines.withContext
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+import kotlin.coroutines.cancellation.CancellationException
+
+/*
+* runSuspendCatching function is designed to execute a suspending function (block)
+* within a coroutine and handle any exceptions that might occur during its execution.
+* It returns a Result object, which encapsulates either the successful result of the
+* function or the exception that was thrown.
+*/
+
+@OptIn(ExperimentalContracts::class)
+suspend inline fun <R> runSuspendCatching(
+  context: CoroutineContext = EmptyCoroutineContext,
+  crossinline block: suspend () -> R
+): Result<R> {
+  contract { callsInPlace(block, InvocationKind.EXACTLY_ONCE) }
+  
+  return try {
+    Result.success(withContext(context) { block() })
+  } catch (c: CancellationException) {
+    throw c
+  } catch (e: Exception) {
+    Result.failure(e)
+  }
+}
