15d6400e6a9060cbeeae110c35b8ead7db4fe34f
MeansKane <phamminhkhangpy2003@gmail.com>
2024-11-03 00:29:58 +0700

    Implement MovieListRepository to fetch and map movie data with CoroutineDispatcher IO

--- app/src/main/java/com/example/moviedocs/data/repository/MovieListRepositoryImpl.kt
+++ app/src/main/java/com/example/moviedocs/data/repository/MovieListRepositoryImpl.kt
@@ -2,22 +2,38 @@ package com.example.moviedocs.data.repository
 
 import com.example.moviedocs.data.mapper.toMovieListModel
 import com.example.moviedocs.data.remote.ApiService
+import com.example.moviedocs.di.AppDispatcher
+import com.example.moviedocs.di.DispatcherType
 import com.example.moviedocs.domain.model.MovieModel
 import com.example.moviedocs.domain.repository.MovieListRepository
+import com.example.moviedocs.utils.runSuspendCatching
+import kotlinx.coroutines.CoroutineDispatcher
 import javax.inject.Inject
 
 class MovieListRepositoryImpl
-@Inject constructor(private val apiService: ApiService) : MovieListRepository {
+@Inject constructor(
+  private val apiService: ApiService,
+  @AppDispatcher(DispatcherType.IO)
+  private val ioDispatcher: CoroutineDispatcher
+) : MovieListRepository {
   
-  override suspend fun getMoviesNowPlaying(): List<MovieModel> =
-    apiService.getMovieNowPlaying().results.map { it.toMovieListModel() }
+  override suspend fun getMoviesNowPlaying(): Result<List<MovieModel>> =
+    runSuspendCatching(ioDispatcher) {
+      apiService.getMovieNowPlaying().results.map { it.toMovieListModel() }
+    }
   
-  override suspend fun getMoviesPopular(): List<MovieModel> =
-    apiService.getMoviePopular().results.map { it.toMovieListModel() }
+  override suspend fun getMoviesPopular(): Result<List<MovieModel>> =
+    runSuspendCatching(ioDispatcher) {
+      apiService.getMoviePopular().results.map { it.toMovieListModel() }
+    }
   
-  override suspend fun getMoviesTopRated(): List<MovieModel> =
-    apiService.getMovieTopRated().results.map { it.toMovieListModel() }
+  override suspend fun getMoviesTopRated(): Result<List<MovieModel>> =
+    runSuspendCatching(ioDispatcher) {
+      apiService.getMovieTopRated().results.map { it.toMovieListModel() }
+    }
   
-  override suspend fun getMoviesUpcoming(): List<MovieModel> =
-    apiService.getMovieUpcoming().results.map { it.toMovieListModel() }
+  override suspend fun getMoviesUpcoming(): Result<List<MovieModel>> =
+    runSuspendCatching(ioDispatcher) {
+      apiService.getMovieUpcoming().results.map { it.toMovieListModel() }
+    }
 }
