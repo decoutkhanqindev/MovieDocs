3e6c4e13135c02360312340a754a6608b2827d17
MeansKane <phamminhkhangpy2003@gmail.com>
2024-11-02 22:40:15 +0700

    Add ApiService interface for fetching movie data

--- app/src/main/java/com/example/moviedocs/data/remote/ApiService.kt
+++ app/src/main/java/com/example/moviedocs/data/remote/ApiService.kt
@@ -1,18 +1,18 @@
 package com.example.moviedocs.data.remote
 
-import com.example.moviedocs.data.remote.response.list.MovieResponse
+import com.example.moviedocs.data.remote.response.list.MovieListResponse
 import retrofit2.http.GET
 
 interface ApiService {
   @GET("movie/now_playing")
-  suspend fun getMovieNowPlaying(): List<MovieResponse>
+  suspend fun getMovieNowPlaying(): MovieListResponse
   
   @GET("movie/popular")
-  suspend fun getMoviePopular(): List<MovieResponse>
+  suspend fun getMoviePopular(): MovieListResponse
   
   @GET("movie/top_rated")
-  suspend fun getMovieTopRated(): List<MovieResponse>
+  suspend fun getMovieTopRated(): MovieListResponse
   
   @GET("movie/upcoming")
-  suspend fun getMovieUpcoming(): List<MovieResponse>
+  suspend fun getMovieUpcoming(): MovieListResponse
 }
