7ba8b925c0ce5353143fde69573522e42732664d
MeansKane <phamminhkhangpy2003@gmail.com>
2024-11-03 00:27:26 +0700

    Add CoroutineModule to provide CoroutineDispatchers with qualifiers

---
+++ app/src/main/java/com/example/moviedocs/di/CoroutineModule.kt
@@ -0,0 +1,37 @@
+package com.example.moviedocs.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Qualifier
+import javax.inject.Singleton
+
+enum class DispatcherType {
+  Default, IO, Main
+}
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class AppDispatcher(val type: DispatcherType)
+
+@Module
+@InstallIn(SingletonComponent::class)
+object CoroutineModule {
+  @Provides
+  @Singleton
+  @AppDispatcher(DispatcherType.Default)
+  fun provideDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
+  
+  @Provides
+  @Singleton
+  @AppDispatcher(DispatcherType.IO)
+  fun provideIODispatcher(): CoroutineDispatcher = Dispatchers.IO
+  
+  @Provides
+  @Singleton
+  @AppDispatcher(DispatcherType.Main)
+  fun provideMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
+}
