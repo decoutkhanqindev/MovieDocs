0353f92f5449cb0d4e21816157a693118c8481df
MeansKane <phamminhkhangpy2003@gmail.com>
2024-11-03 00:29:07 +0700

    Add MovieListRepository interface for fetching movie lists with obj Result

--- app/src/main/java/com/example/moviedocs/domain/repository/MovieListRepository.kt
+++ app/src/main/java/com/example/moviedocs/domain/repository/MovieListRepository.kt
@@ -3,8 +3,8 @@ package com.example.moviedocs.domain.repository
 import com.example.moviedocs.domain.model.MovieModel
 
 interface MovieListRepository {
-  suspend fun getMoviesNowPlaying(): List<MovieModel>
-  suspend fun getMoviesPopular(): List<MovieModel>
-  suspend fun getMoviesTopRated(): List<MovieModel>
-  suspend fun getMoviesUpcoming(): List<MovieModel>
+  suspend fun getMoviesNowPlaying(): Result<List<MovieModel>>
+  suspend fun getMoviesPopular(): Result<List<MovieModel>>
+  suspend fun getMoviesTopRated(): Result<List<MovieModel>>
+  suspend fun getMoviesUpcoming(): Result<List<MovieModel>>
 }
