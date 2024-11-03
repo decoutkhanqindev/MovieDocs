64e531ea0c7e268c273ea15080b924962af796b9
MeansKane <phamminhkhangpy2003@gmail.com>
2024-11-04 01:23:11 +0700

    Add mapper to convert MovieResponse to MovieModel

--- app/src/main/java/com/example/moviedocs/data/mapper/MovieListMapper.kt
+++ app/src/main/java/com/example/moviedocs/data/mapper/MovieListMapper.kt
@@ -1,9 +1,10 @@
 package com.example.moviedocs.data.mapper
 
 import com.example.moviedocs.data.remote.response.list.MovieListResponse
+import com.example.moviedocs.data.remote.response.list.MovieResponse
 import com.example.moviedocs.domain.model.MovieModel
 
-fun MovieListResponse.Movie.toMovieListModel(): MovieModel =
+fun MovieResponse.toMovieModel(): MovieModel =
   MovieModel(
     id = id,
     title = title,
