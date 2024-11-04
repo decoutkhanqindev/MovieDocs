e57c0cd6066fab3849050613ef39c590c6de5c44
MeansKane <phamminhkhangpy2003@gmail.com>
2024-11-05 02:18:09 +0700

    Add MovieModelItemCallBack for DiffUtil in slider adapter

---
+++ app/src/main/java/com/example/moviedocs/presentation/list/slider/MovieModelItemCallBack.kt
@@ -0,0 +1,14 @@
+package com.example.moviedocs.presentation.list.slider
+
+import androidx.recyclerview.widget.DiffUtil
+import com.example.moviedocs.domain.model.MovieModel
+
+object MovieModelItemCallBack : DiffUtil.ItemCallback<MovieModel>() {
+  override fun areContentsTheSame(
+    oldItem: MovieModel, newItem: MovieModel
+  ): Boolean = oldItem == newItem
+  
+  override fun areItemsTheSame(
+    oldItem: MovieModel, newItem: MovieModel
+  ): Boolean = oldItem.id == newItem.id
+}
