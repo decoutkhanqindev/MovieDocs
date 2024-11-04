bf1872a626477dcd5eaa33ea901e4f8afd5db413
MeansKane <phamminhkhangpy2003@gmail.com>
2024-11-04 16:38:16 +0700

    Add get all movie list use case to fetch data

--- app/src/main/java/com/example/moviedocs/domain/usecase/list/GetMovieListPopularUseCase.kt
+++ app/src/main/java/com/example/moviedocs/domain/usecase/list/GetMovieListPopularUseCase.kt
@@ -4,9 +4,9 @@ import com.example.moviedocs.domain.model.MovieModel
 import com.example.moviedocs.domain.repository.MovieListRepository
 import javax.inject.Inject
 
-class GetMovieListNowPlayingUseCase
+class GetMovieListPopularUseCase
 @Inject constructor(private val repository: MovieListRepository) {
   suspend operator fun invoke(page: Int): Result<List<MovieModel>> =
-    repository.getMoviesNowPlaying(page = page)
+    repository.getMoviesPopular(page = page)
 }
 
--- app/src/main/java/com/example/moviedocs/domain/usecase/list/GetMovieListTopRatedUseCase.kt
+++ app/src/main/java/com/example/moviedocs/domain/usecase/list/GetMovieListTopRatedUseCase.kt
@@ -4,9 +4,9 @@ import com.example.moviedocs.domain.model.MovieModel
 import com.example.moviedocs.domain.repository.MovieListRepository
 import javax.inject.Inject
 
-class GetMovieListNowPlayingUseCase
+class GetMovieListTopRatedUseCase
 @Inject constructor(private val repository: MovieListRepository) {
   suspend operator fun invoke(page: Int): Result<List<MovieModel>> =
-    repository.getMoviesNowPlaying(page = page)
+    repository.getMoviesTopRated(page = page)
 }
 
