947160249e26916c2ae0653054594c54cbd13215
MeansKane <phamminhkhangpy2003@gmail.com>
2024-11-05 02:17:45 +0700

    feat: Implement SliderAdapter for displaying movie sliders in ViewPager2

---
+++ app/src/main/java/com/example/moviedocs/presentation/list/slider/SliderAdapter.kt
@@ -0,0 +1,51 @@
+package com.example.moviedocs.presentation.list.slider
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
+import com.bumptech.glide.RequestManager
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.example.moviedocs.databinding.SliderItemViewHolderBinding
+import com.example.moviedocs.domain.model.MovieModel
+
+class SliderAdapter(
+  private val viewPager: ViewPager2,
+  private val requestManager: RequestManager,
+//  private val onItemClicked: (MovieModel) -> Unit
+) : ListAdapter<MovieModel, SliderAdapter.SliderViewHolder>(MovieModelItemCallBack) {
+  
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SliderViewHolder =
+    SliderViewHolder(
+      SliderItemViewHolderBinding.inflate(
+        /* inflater = */ LayoutInflater.from(parent.context),
+        /* parent = */ parent,
+        /* attachToParent = */ false
+      )
+    )
+  
+  override fun onBindViewHolder(holder: SliderViewHolder, position: Int) =
+    holder.bind(getItem(position))
+  
+  inner class SliderViewHolder(
+    private val binding: SliderItemViewHolderBinding
+  ) : RecyclerView.ViewHolder(binding.root) {
+    
+    fun bind(item: MovieModel) {
+      binding.run {
+        val imageUrl = "https://image.tmdb.org/t/p/w500${item.posterPath}"
+        requestManager.load(imageUrl)
+          .fitCenter()
+          .centerCrop()
+          .transition(DrawableTransitionOptions.withCrossFade())
+          .into(sliderItemImg)
+        
+        sliderItemTitleText.text = item.title
+      }
+    }
+  }
+}
+
+
+
