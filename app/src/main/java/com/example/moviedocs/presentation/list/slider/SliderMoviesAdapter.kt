7f45e55b004d41de0c6a5b5b8b9f31e49afe7811
MeansKane <phamminhkhangpy2003@gmail.com>
2024-11-07 01:02:24 +0700

    Handle infinite scrolling slider with ViewPager2

--- app/src/main/java/com/example/moviedocs/presentation/list/slider/SliderMoviesAdapter.kt
+++ app/src/main/java/com/example/moviedocs/presentation/list/slider/SliderMoviesAdapter.kt
@@ -4,25 +4,33 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
 import com.example.moviedocs.databinding.SliderItemViewHolderBinding
 import com.example.moviedocs.domain.model.MovieModel
 import com.example.moviedocs.utils.loadImgFromUrl
 
 class SliderMoviesAdapter(
+  private val viewPager: ViewPager2,
 //  private val onItemClicked: (MovieModel) -> Unit
 ) : ListAdapter<MovieModel, SliderMoviesAdapter.SliderViewHolder>(MovieModelItemCallBack) {
   
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SliderViewHolder =
     SliderViewHolder(
-      SliderItemViewHolderBinding.inflate(
-        /* inflater = */ LayoutInflater.from(parent.context),
-        /* parent = */ parent,
-        /* attachToParent = */ false
+      SliderItemViewHolderBinding.inflate(/* inflater = */ LayoutInflater.from(parent.context),/* parent = */
+        parent,/* attachToParent = */
+        false
       )
     )
   
-  override fun onBindViewHolder(holder: SliderViewHolder, position: Int) =
+  override fun onBindViewHolder(holder: SliderViewHolder, position: Int) {
     holder.bind(getItem(position))
+    // infinite scrolling
+    if (position == currentList.lastIndex) {
+      viewPager.post {
+        viewPager.setCurrentItem(0, true)
+      }
+    }
+  }
   
   inner class SliderViewHolder(
     private val binding: SliderItemViewHolderBinding
