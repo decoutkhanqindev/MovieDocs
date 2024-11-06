9c88240ca3794e0a3aec27f3ed745638d4f3d67e
MeansKane <phamminhkhangpy2003@gmail.com>
2024-11-06 23:36:16 +0700

    Update project

--- app/src/main/java/com/example/moviedocs/presentation/list/MovieListSingleEvent.kt
+++
@@ -1,6 +0,0 @@
-package com.example.moviedocs.presentation.list
-
-sealed interface MovieListSingleEvent {
-  data object Success : MovieListSingleEvent
-  data class Error(val throwable: Throwable) : MovieListSingleEvent
-}
