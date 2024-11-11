de60f8a77e202fdb5b86fbfc4140f318879da8a5
MeansKane <phamminhkhangpy2003@gmail.com>
2024-11-12 00:39:27 +0700

    Handle to display bottom navigation menu and navigate between each item

--- app/src/main/java/com/example/moviedocs/presentation/MainActivity.kt
+++ app/src/main/java/com/example/moviedocs/presentation/MainActivity.kt
@@ -4,12 +4,18 @@ import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.moviedocs.R
 import com.example.moviedocs.databinding.ActivityMainBinding
 import com.example.moviedocs.databinding.CustomNetworkDialogLayoutBinding
 import com.example.moviedocs.presentation.base.BaseActivity
 import com.example.moviedocs.utils.connectivity.NetworkConnectivityObserver
+import com.example.moviedocs.utils.gone
 import com.example.moviedocs.utils.launchAndRepeatStarted
+import com.example.moviedocs.utils.visible
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -19,14 +25,32 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
   @Inject
   internal lateinit var networkConnectivityObserver: NetworkConnectivityObserver
   
-  private var alertDialog: AlertDialog? = null
-  
   private lateinit var navController: NavController
+  
+  private var alertDialog: AlertDialog? = null
   
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     
+    setUpNavigation()
     observeNetworkConnectivity()
+  }
+  
+  private fun setUpNavigation() {
+    val navHostFragment: NavHostFragment = binding.mainNavHost.getFragment()
+    navController = navHostFragment.navController
+    binding.bottomNavigationMenu.setupWithNavController(navController)
+    
+    navController.addOnDestinationChangedListener { _: NavController, destination: NavDestination, _: Bundle? ->
+      handleBottomNavigationMenuVisibility(destination.id)
+    }
+  }
+  
+  private fun handleBottomNavigationMenuVisibility(destination: Int) {
+    when (destination) {
+      R.id.homeFragment, R.id.favoriteFragment, R.id.profileFragment -> binding.bottomNavigationMenu.visible()
+      else -> binding.bottomNavigationMenu.gone()
+    }
   }
   
   private fun observeNetworkConnectivity() {
@@ -43,7 +67,8 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
   }
   
   private fun showAlertDialog() {
-    val dialogLayoutBinding: CustomNetworkDialogLayoutBinding = CustomNetworkDialogLayoutBinding.inflate(layoutInflater)
+    val dialogLayoutBinding: CustomNetworkDialogLayoutBinding =
+      CustomNetworkDialogLayoutBinding.inflate(layoutInflater)
     
     if (alertDialog?.isShowing == true) return
     alertDialog = AlertDialog.Builder(this)
