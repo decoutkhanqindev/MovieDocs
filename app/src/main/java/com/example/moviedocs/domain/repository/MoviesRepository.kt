03ad7d893000590043fccde600db93f2122b1980
MeansKane <phamminhkhangpy2003@gmail.com>
2024-11-12 23:16:34 +0700

    Update project

--- app/src/main/java/com/example/moviedocs/domain/repository/MoviesRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.moviedocs.domain.repository
-
-import com.example.moviedocs.domain.model.MovieModel
-
-interface MoviesRepository {
-  suspend fun getNowPlayingMovies(page: Int): Result<List<MovieModel>>
-  suspend fun getPopularMovies(page: Int): Result<List<MovieModel>>
-  suspend fun getUpcomingMovies(page: Int): Result<List<MovieModel>>
-}
