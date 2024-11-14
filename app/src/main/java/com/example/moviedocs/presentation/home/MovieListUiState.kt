2ee8044828add3273f5afce26d9bfdf4840dbc33
MeansKane <phamminhkhangpy2003@gmail.com>
2024-11-15 00:53:04 +0700

    Handle each movie list to display more movies with load more page by view model

--- app/src/main/java/com/example/moviedocs/presentation/home/MovieListUiState.kt
+++ app/src/main/java/com/example/moviedocs/presentation/home/MovieListUiState.kt
@@ -1,17 +1,18 @@
 package com.example.moviedocs.presentation.home
 
-import com.example.moviedocs.domain.model.MovieModel
+import com.example.moviedocs.domain.model.list.MovieItemModel
 
 sealed interface MovieListUiState {
   data object FirstPageLoading : MovieListUiState
   data object FirstPageError : MovieListUiState
+  
   data class Success(
-    val items: List<MovieModel>,
+    val items: List<MovieItemModel>,
     val currentPage: Int,
-    val nextPageState: MovieListNextPageState
+    val nextPageState: NextPageState,
   ) : MovieListUiState
   
-  enum class MovieListNextPageState {
+  enum class NextPageState {
     LOADING,  // load more a next page state is loading
     ERROR, // load more a next page state is error
     LOAD_MORE, // load more a next page state is idle -> it scrolled a last element -> load more
