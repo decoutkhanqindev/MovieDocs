c50aff129f66d5d2e85f2f57d26faeb96f0d3487
MeansKane <phamminhkhangpy2003@gmail.com>
2024-11-15 00:54:34 +0700

    Update project

--- app/src/main/java/com/example/moviedocs/data/remote/ApiService.kt
+++ app/src/main/java/com/example/moviedocs/data/remote/ApiService.kt
@@ -6,14 +6,14 @@ import retrofit2.http.Query
 
 interface ApiService {
   @GET("movie/now_playing")
-  suspend fun getNowPlaying(@Query("page") page: Int = 1): MovieListResponse
+  suspend fun getNowPlaying(@Query("page") page: Int): MovieListResponse
   
   @GET("movie/popular")
-  suspend fun getPopular(@Query("page") page: Int = 1): MovieListResponse
+  suspend fun getPopular(@Query("page") page: Int): MovieListResponse
   
   @GET("movie/upcoming")
-  suspend fun getUpcoming(@Query("page") page: Int = 1): MovieListResponse
+  suspend fun getUpcoming(@Query("page") page: Int): MovieListResponse
   
   @GET("movie/top_rated")
-  suspend fun getTopRated(@Query("page") page: Int = 1): MovieListResponse
+  suspend fun getTopRated(@Query("page") page: Int): MovieListResponse
 }
