28f0c2300a78a713270d62d8440b9152434a08a5
MeansKane <phamminhkhangpy2003@gmail.com>
2024-11-15 01:45:43 +0700

    Update project

--- app/src/main/java/com/example/moviedocs/MovieDocsApplication.kt
+++ app/src/main/java/com/example/moviedocs/MovieDocsApplication.kt
@@ -2,6 +2,15 @@ package com.example.moviedocs
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class MovieDocsApplication : Application()
+class MovieDocsApplication : Application() {
+  override fun onCreate() {
+    super.onCreate()
+    
+    if (BuildConfig.DEBUG) {
+      Timber.plant(Timber.DebugTree())
+    }
+  }
+}
