165fbde648ad91d2c84f8a212a6eee540f08f1b7
MeansKane <phamminhkhangpy2003@gmail.com>
2024-11-16 00:39:56 +0700

    Update project

--- app/src/main/java/com/example/moviedocs/presentation/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/example/moviedocs/presentation/favorite/FavoriteFragment.kt
@@ -3,5 +3,9 @@ package com.example.moviedocs.presentation.favorite
 import com.example.moviedocs.databinding.FragmentFavoriteBinding
 import com.example.moviedocs.presentation.base.BaseFragment
 
-class FavoriteFragment : BaseFragment<FragmentFavoriteBinding>(FragmentFavoriteBinding::inflate){
+class FavoriteFragment : BaseFragment<FragmentFavoriteBinding>(FragmentFavoriteBinding::inflate) {
+  
+  companion object {
+    fun newInstance(): FavoriteFragment = FavoriteFragment()
+  }
 }
