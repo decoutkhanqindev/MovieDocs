b9c9928d4d0c8276b5d39a0f7ee8e2c45dc38a15
MeansKane <phamminhkhangpy2003@gmail.com>
2024-11-23 01:28:32 +0700

    Add GenreListModel represents for each genre list, it include GenreItemModel

---
+++ app/src/main/java/com/example/moviedocs/data/remote/response/genre/GenreItemResponse.kt
@@ -0,0 +1,12 @@
+package com.example.moviedocs.data.remote.response.genre
+
+import androidx.annotation.Keep
+import com.squareup.moshi.Json
+
+@Keep
+data class GenreItemResponse(
+  @Json(name = "id")
+  val id: Int, // 28
+  @Json(name = "name")
+  val name: String, // Action
+)
---
+++ app/src/main/java/com/example/moviedocs/domain/model/genre/GenreItemModel.kt
@@ -0,0 +1,6 @@
+package com.example.moviedocs.domain.model.genre
+
+data class GenreItemModel(
+  val id: Int,
+  val name: String,
+)
