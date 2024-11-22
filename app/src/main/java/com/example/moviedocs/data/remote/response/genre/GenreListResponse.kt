d776e78aa25e0851a2b3aebded4b7b9a022e4efc
MeansKane <phamminhkhangpy2003@gmail.com>
2024-11-23 01:39:20 +0700

    Add get genre list use case

---
+++ app/src/main/java/com/example/moviedocs/data/remote/response/genre/GenreListResponse.kt
@@ -0,0 +1,12 @@
+package com.example.moviedocs.data.remote.response.genre
+
+
+import androidx.annotation.Keep
+import com.squareup.moshi.Json
+
+@Keep
+data class GenreListResponse(
+  @Json(name = "genres")
+  val genres: List<GenreItemResponse>,
+)
+
