43c931b0e61d3316642a3e421ed57a40682240d1
MeansKane <phamminhkhangpy2003@gmail.com>
2024-11-23 01:39:33 +0700

    Add repository for genre list

--- app/src/main/java/com/example/moviedocs/di/RepositoryModule.kt
+++ app/src/main/java/com/example/moviedocs/di/RepositoryModule.kt
@@ -1,7 +1,9 @@
 package com.example.moviedocs.di
 
-import com.example.moviedocs.data.repository.MovieListRepositoryImpl
-import com.example.moviedocs.domain.repository.MovieListRepository
+import com.example.moviedocs.data.repository.genre.GenreListRepositoryImpl
+import com.example.moviedocs.data.repository.list.MovieListRepositoryImpl
+import com.example.moviedocs.domain.repository.genre.GenreListRepository
+import com.example.moviedocs.domain.repository.list.MovieListRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -12,4 +14,7 @@ import dagger.hilt.components.SingletonComponent
 interface RepositoryModule {
   @Binds
   fun bindMovieListRepository(repository: MovieListRepositoryImpl): MovieListRepository
+  
+  @Binds
+  fun bindGenreListRepository(repository: GenreListRepositoryImpl): GenreListRepository
 }
