c1d015fd9e1fae1ae9e3851c61d62371389985dc
MeansKane <phamminhkhangpy2003@gmail.com>
2024-11-26 23:17:18 +0700

    Add genre list adapter for display on recyclerview

--- app/src/main/java/com/example/moviedocs/presentation/moviegenre/GenreListAdapter.kt
+++ app/src/main/java/com/example/moviedocs/presentation/moviegenre/GenreListAdapter.kt
@@ -1,4 +1,4 @@
-package com.example.moviedocs.presentation.adapter.moviegenre
+package com.example.moviedocs.presentation.moviegenre
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
@@ -7,7 +7,7 @@ import com.example.moviedocs.databinding.GenreItemViewHolderBinding
 import com.example.moviedocs.domain.model.moviegenre.GenreItemModel
 import com.example.moviedocs.presentation.base.BaseListAdapter
 
-class GenreListHorizontalAdapter :
+class GenreListAdapter :
   BaseListAdapter<GenreItemModel, GenreItemViewHolderBinding>(GenreItemModelDiffCallBack) {
   
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder =
