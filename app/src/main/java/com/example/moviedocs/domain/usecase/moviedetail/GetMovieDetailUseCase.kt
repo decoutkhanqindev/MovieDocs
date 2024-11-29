0c68139693ba86987ed21d097affd6dc7cc9e55f
MeansKane <phamminhkhangpy2003@gmail.com>
2024-11-30 00:28:18 +0700

    Add movie detail and external ids use case  in domain layer

---
+++ app/src/main/java/com/example/moviedocs/domain/usecase/moviedetail/GetMovieDetailUseCase.kt
@@ -0,0 +1,11 @@
+package com.example.moviedocs.domain.usecase.moviedetail
+
+import com.example.moviedocs.domain.model.moviedetail.MovieDetailModel
+import com.example.moviedocs.domain.repository.moviedetail.MovieDetailRepository
+import javax.inject.Inject
+
+class GetMovieDetailUseCase
+@Inject constructor(private val repository: MovieDetailRepository) {
+  suspend operator fun invoke(movieId: Int): Result<MovieDetailModel> =
+    repository.getMovieDetail(movieId = movieId)
+}
