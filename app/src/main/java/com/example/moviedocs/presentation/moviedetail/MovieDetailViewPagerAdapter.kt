33eeb083373fc04370da038b55c9f17bbfde7d2e
MeansKane <phamminhkhangpy2003@gmail.com>
2024-12-01 00:42:19 +0700

    Add movie detail view pager adapter for create fragment when scrolling to each other

---
+++ app/src/main/java/com/example/moviedocs/presentation/moviedetail/MovieDetailViewPagerAdapter.kt
@@ -0,0 +1,17 @@
+package com.example.moviedocs.presentation.moviedetail
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class MovieDetailViewPagerAdapter(
+  fragment: Fragment,
+) : FragmentStateAdapter(fragment) {
+  
+  override fun getItemCount(): Int = 2
+  
+  override fun createFragment(position: Int): Fragment = when (position) {
+    0 -> MovieDetailOverviewFragment.newInstance()
+    1 -> MovieDetailCastAndCrewFragment.newInstance()
+    else -> throw IllegalArgumentException("Invalid $position")
+  }
+}
