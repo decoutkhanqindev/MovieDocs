3a58a973437a34b472aeb43eac92003c2eeeb7f5
MeansKane <phamminhkhangpy2003@gmail.com>
2024-12-01 00:40:35 +0700

    Add movie detail cast and crew fragment

---
+++ app/src/main/java/com/example/moviedocs/presentation/moviedetail/MovieDetailCastAndCrewFragment.kt
@@ -0,0 +1,19 @@
+package com.example.moviedocs.presentation.moviedetail
+
+import android.os.Bundle
+import android.view.View
+import com.example.moviedocs.databinding.FragmentMovieDetailOverviewBinding
+import com.example.moviedocs.presentation.base.BaseFragment
+
+class MovieDetailCastAndCrewFragment : BaseFragment<FragmentMovieDetailOverviewBinding>(
+  FragmentMovieDetailOverviewBinding::inflate
+) {
+  
+  companion object {
+    fun newInstance(): MovieDetailCastAndCrewFragment = MovieDetailCastAndCrewFragment()
+  }
+  
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+  }
+}
