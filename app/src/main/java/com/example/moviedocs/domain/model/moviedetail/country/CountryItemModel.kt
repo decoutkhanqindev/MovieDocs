7c4de88fcfaf99f40ef59c3593d03c265dc6cebc
MeansKane <phamminhkhangpy2003@gmail.com>
2024-12-03 00:49:56 +0700

    Add country and language item model

---
+++ app/src/main/java/com/example/moviedocs/domain/model/moviedetail/country/CountryItemModel.kt
@@ -0,0 +1,7 @@
+package com.example.moviedocs.domain.model.moviedetail.country
+
+data class CountryItemModel(
+  val iso31661: String, // AD
+  val englishName: String, // Andorra
+  val nativeName: String, // Andorra
+)
---
+++ app/src/main/java/com/example/moviedocs/domain/model/moviedetail/language/LanguageItemModel.kt
@@ -0,0 +1,7 @@
+package com.example.moviedocs.domain.model.moviedetail.language
+
+data class LanguageItemModel(
+  val iso6391: String, // kw
+  val englishName: String, // Cornish
+  val name: String,
+)
