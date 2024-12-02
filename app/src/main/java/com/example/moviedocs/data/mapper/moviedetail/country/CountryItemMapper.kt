cae8d0b717f5d7be7e00a9a5c135ef1b91d2b73c
MeansKane <phamminhkhangpy2003@gmail.com>
2024-12-03 00:51:51 +0700

    Add country and language item mapper to model

---
+++ app/src/main/java/com/example/moviedocs/data/mapper/moviedetail/country/CountryItemMapper.kt
@@ -0,0 +1,11 @@
+package com.example.moviedocs.data.mapper.moviedetail.country
+
+import com.example.moviedocs.data.remote.response.moviedetails.counttry.CountryItemResponse
+import com.example.moviedocs.domain.model.moviedetail.country.CountryItemModel
+
+fun CountryItemResponse.toCountryItemModel(): CountryItemModel =
+  CountryItemModel(
+    iso31661 = iso31661,
+    englishName = englishName,
+    nativeName = nativeName,
+  )
