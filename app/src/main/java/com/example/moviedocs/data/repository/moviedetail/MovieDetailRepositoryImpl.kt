2bf44aea6d8bc9713a5395ee0d9ffb720dcabb9a
MeansKane <phamminhkhangpy2003@gmail.com>
2024-12-03 00:50:50 +0700

    Add country and language list in movie detail repository

--- app/src/main/java/com/example/moviedocs/data/repository/moviedetail/MovieDetailRepositoryImpl.kt
+++ app/src/main/java/com/example/moviedocs/data/repository/moviedetail/MovieDetailRepositoryImpl.kt
@@ -1,12 +1,16 @@
 package com.example.moviedocs.data.repository.moviedetail
 
+import com.example.moviedocs.data.mapper.moviedetail.country.toCountryItemModel
 import com.example.moviedocs.data.mapper.moviedetail.external.toExternalIdsModel
+import com.example.moviedocs.data.mapper.moviedetail.language.toLanguageItemModel
 import com.example.moviedocs.data.mapper.moviedetail.toMovieDetailModel
 import com.example.moviedocs.data.remote.ApiService
 import com.example.moviedocs.di.AppDispatcher
 import com.example.moviedocs.di.DispatcherType
 import com.example.moviedocs.domain.model.moviedetail.MovieDetailModel
+import com.example.moviedocs.domain.model.moviedetail.country.CountryItemModel
 import com.example.moviedocs.domain.model.moviedetail.external.ExternalIdsModel
+import com.example.moviedocs.domain.model.moviedetail.language.LanguageItemModel
 import com.example.moviedocs.domain.repository.moviedetail.MovieDetailRepository
 import com.example.moviedocs.utils.runSuspendCatching
 import kotlinx.coroutines.CoroutineDispatcher
@@ -26,5 +30,15 @@ class MovieDetailRepositoryImpl @Inject constructor(
   override suspend fun getExternalIds(movieId: Int): Result<ExternalIdsModel> =
     runSuspendCatching(ioDispatcher) {
       apiService.getExternalIds(movieId = movieId).toExternalIdsModel()
+    }
+  
+  override suspend fun getCountryList(): Result<List<CountryItemModel>> =
+    runSuspendCatching(ioDispatcher) {
+      apiService.getCountryList().map { it.toCountryItemModel() }
+    }
+  
+  override suspend fun getLanguageList(): Result<List<LanguageItemModel>> =
+    runSuspendCatching(ioDispatcher) {
+      apiService.getLanguageList().map { it.toLanguageItemModel() }
     }
 }
--- app/src/main/java/com/example/moviedocs/domain/repository/moviedetail/MovieDetailRepository.kt
+++ app/src/main/java/com/example/moviedocs/domain/repository/moviedetail/MovieDetailRepository.kt
@@ -1,9 +1,13 @@
 package com.example.moviedocs.domain.repository.moviedetail
 
 import com.example.moviedocs.domain.model.moviedetail.MovieDetailModel
+import com.example.moviedocs.domain.model.moviedetail.country.CountryItemModel
 import com.example.moviedocs.domain.model.moviedetail.external.ExternalIdsModel
+import com.example.moviedocs.domain.model.moviedetail.language.LanguageItemModel
 
 interface MovieDetailRepository {
   suspend fun getMovieDetail(movieId: Int): Result<MovieDetailModel>
   suspend fun getExternalIds(movieId: Int): Result<ExternalIdsModel>
+  suspend fun getCountryList(): Result<List<CountryItemModel>>
+  suspend fun getLanguageList(): Result<List<LanguageItemModel>>
 }
