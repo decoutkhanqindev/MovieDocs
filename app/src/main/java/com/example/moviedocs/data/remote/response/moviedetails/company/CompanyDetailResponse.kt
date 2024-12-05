76b665cc1f370852fc5f1c26023813765684ac37
MeansKane <phamminhkhangpy2003@gmail.com>
2024-12-06 00:13:09 +0700

    Add company detail response

---
+++ app/src/main/java/com/example/moviedocs/data/remote/response/moviedetails/company/CompanyDetailResponse.kt
@@ -0,0 +1,24 @@
+package com.example.moviedocs.data.remote.response.moviedetails.company
+
+import androidx.annotation.Keep
+import com.squareup.moshi.Json
+
+@Keep
+data class CompanyDetailResponse(
+  @Json(name = "description")
+  val description: String?,
+  @Json(name = "headquarters")
+  val headquarters: String?,
+  @Json(name = "homepage")
+  val homepage: String?,
+  @Json(name = "id")
+  val id: Int,
+  @Json(name = "logo_path")
+  val logoPath: String?,
+  @Json(name = "name")
+  val name: String?,
+  @Json(name = "origin_country")
+  val originCountry: String?,
+  @Json(name = "parent_company")
+  val parentCompany: String?
+)
