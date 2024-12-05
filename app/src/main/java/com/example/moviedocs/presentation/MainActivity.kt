a5d83c29a1e3b5ab6fd151b07a1e3a927f04a76e
MeansKane <phamminhkhangpy2003@gmail.com>
2024-12-06 00:12:22 +0700

    Update project

--- app/src/main/java/com/example/moviedocs/presentation/MainActivity.kt
+++ app/src/main/java/com/example/moviedocs/presentation/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.moviedocs.presentation
 import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -20,25 +23,36 @@ import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
-  
+
   @Inject
   internal lateinit var networkConnectivityObserver: NetworkConnectivityObserver
-  
+
   private var alertDialog: AlertDialog? = null
-  
+
   private lateinit var navController: NavController
-  
+
   override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
     super.onCreate(savedInstanceState)
-    
+    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+      val systemBarsInsets = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+      v.setPadding(
+        systemBarsInsets.left,
+        systemBarsInsets.top,
+        systemBarsInsets.right,
+        systemBarsInsets.bottom
+      )
+      insets
+    }
+
     setUpBottomNavigationMenu()
     observeNetworkConnectivity()
   }
-  
+
   private fun setUpBottomNavigationMenu() {
     val navHostFragment: NavHostFragment = binding.mainNavHost.getFragment()
     navController = navHostFragment.navController
-    
+
     navController.addOnDestinationChangedListener { _, destination, _ ->
       when (destination.id) {
         R.id.homeFragment, R.id.favoriteFragment, R.id.profileFragment -> {
@@ -47,47 +61,47 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
             binding.bottomNavigationMenu.visible()
           })
         }
-        
+
         else -> binding.bottomNavigationMenu.gone()
       }
     }
-    
+
     binding.bottomNavigationMenu.setupWithNavController(navController)
   }
-  
+
   override fun onSupportNavigateUp(): Boolean {
     return navController.navigateUp() || super.onSupportNavigateUp()
   }
-  
+
   private fun observeNetworkConnectivity() {
     launchAndRepeatStarted(
       { networkConnectivityObserver.observe().collect(::renderAlertDialog) },
     )
   }
-  
+
   private fun renderAlertDialog(status: NetworkConnectivityObserver.NetworkStatus) {
     when (status) {
       NetworkConnectivityObserver.NetworkStatus.AVAILABLE -> hideAlertDialog()
       else -> showAlertDialog()
     }
   }
-  
+
   private fun showAlertDialog() {
     val dialogLayoutBinding: CustomNetworkDialogLayoutBinding =
       CustomNetworkDialogLayoutBinding.inflate(layoutInflater)
-    
+
     if (alertDialog?.isShowing == true) return
     alertDialog = AlertDialog.Builder(this).setView(dialogLayoutBinding.root)
       .setCancelable(false) // can not close a dialog if network is unavailable
       .show()
-    
+
     dialogLayoutBinding.tryAgainBtn.setOnClickListener {
       // open wifi settings
       val intent = Intent(android.provider.Settings.ACTION_WIFI_SETTINGS)
       startActivity(intent)
     }
   }
-  
+
   private fun hideAlertDialog() {
     alertDialog?.dismiss()
     alertDialog = null
