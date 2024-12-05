0f7c401fef655a06f7ad77506a971aeb1769b896
MeansKane <phamminhkhangpy2003@gmail.com>
2024-12-06 00:15:05 +0700

    Add get company detail in movie detail repository

--- app/src/main/java/com/example/moviedocs/data/repository/moviedetail/MovieDetailRepositoryImpl.kt
+++ app/src/main/java/com/example/moviedocs/data/repository/moviedetail/MovieDetailRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.example.moviedocs.data.repository.moviedetail
 
+import com.example.moviedocs.data.mapper.moviedetail.company.toCompanyDetailModel
 import com.example.moviedocs.data.mapper.moviedetail.country.toCountryItemModel
 import com.example.moviedocs.data.mapper.moviedetail.external.toExternalIdsModel
 import com.example.moviedocs.data.mapper.moviedetail.language.toLanguageItemModel
@@ -8,6 +9,7 @@ import com.example.moviedocs.data.remote.ApiService
 import com.example.moviedocs.di.AppDispatcher
 import com.example.moviedocs.di.DispatcherType
 import com.example.moviedocs.domain.model.moviedetail.MovieDetailModel
+import com.example.moviedocs.domain.model.moviedetail.company.CompanyDetailModel
 import com.example.moviedocs.domain.model.moviedetail.country.CountryItemModel
 import com.example.moviedocs.domain.model.moviedetail.external.ExternalIdsModel
 import com.example.moviedocs.domain.model.moviedetail.language.LanguageItemModel
@@ -21,24 +23,29 @@ class MovieDetailRepositoryImpl @Inject constructor(
   @AppDispatcher(DispatcherType.IO)
   private val ioDispatcher: CoroutineDispatcher,
 ) : MovieDetailRepository {
-  
+
   override suspend fun getMovieDetail(movieId: Int): Result<MovieDetailModel> =
     runSuspendCatching(ioDispatcher) {
       apiService.getMovieDetail(movieId = movieId).toMovieDetailModel()
     }
-  
+
   override suspend fun getExternalIds(movieId: Int): Result<ExternalIdsModel> =
     runSuspendCatching(ioDispatcher) {
       apiService.getExternalIds(movieId = movieId).toExternalIdsModel()
     }
-  
+
   override suspend fun getCountryList(): Result<List<CountryItemModel>> =
     runSuspendCatching(ioDispatcher) {
       apiService.getCountryList().map { it.toCountryItemModel() }
     }
-  
+
   override suspend fun getLanguageList(): Result<List<LanguageItemModel>> =
     runSuspendCatching(ioDispatcher) {
       apiService.getLanguageList().map { it.toLanguageItemModel() }
+    }
+
+  override suspend fun getCompanyDetail(companyId: Int): Result<CompanyDetailModel> =
+    runSuspendCatching(ioDispatcher) {
+      apiService.getCompanyDetail(companyId = companyId).toCompanyDetailModel()
     }
 }
--- app/src/main/java/com/example/moviedocs/domain/repository/moviedetail/MovieDetailRepository.kt
+++ app/src/main/java/com/example/moviedocs/domain/repository/moviedetail/MovieDetailRepository.kt
@@ -1,6 +1,7 @@
 package com.example.moviedocs.domain.repository.moviedetail
 
 import com.example.moviedocs.domain.model.moviedetail.MovieDetailModel
+import com.example.moviedocs.domain.model.moviedetail.company.CompanyDetailModel
 import com.example.moviedocs.domain.model.moviedetail.country.CountryItemModel
 import com.example.moviedocs.domain.model.moviedetail.external.ExternalIdsModel
 import com.example.moviedocs.domain.model.moviedetail.language.LanguageItemModel
@@ -10,4 +11,5 @@ interface MovieDetailRepository {
   suspend fun getExternalIds(movieId: Int): Result<ExternalIdsModel>
   suspend fun getCountryList(): Result<List<CountryItemModel>>
   suspend fun getLanguageList(): Result<List<LanguageItemModel>>
+  suspend fun getCompanyDetail(companyId: Int): Result<CompanyDetailModel>
 }
