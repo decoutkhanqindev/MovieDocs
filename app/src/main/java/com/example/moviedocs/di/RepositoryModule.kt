7dead50da3703f08aa52953bf689717d7718ec4a
MeansKane <phamminhkhangpy2003@gmail.com>
2024-12-09 00:26:56 +0700

    Add country repository to repository module

--- app/src/main/java/com/example/moviedocs/di/RepositoryModule.kt
+++ app/src/main/java/com/example/moviedocs/di/RepositoryModule.kt
@@ -1,8 +1,14 @@
 package com.example.moviedocs.di
 
+import com.example.moviedocs.data.repository.company.CompanyRepositoryImpl
+import com.example.moviedocs.data.repository.country.CountryRepositoryImpl
+import com.example.moviedocs.data.repository.language.LanguageRepositoryImpl
 import com.example.moviedocs.data.repository.moviedetail.MovieDetailRepositoryImpl
 import com.example.moviedocs.data.repository.moviegenre.GenreListRepositoryImpl
 import com.example.moviedocs.data.repository.movielist.MovieListRepositoryImpl
+import com.example.moviedocs.domain.repository.company.CompanyRepository
+import com.example.moviedocs.domain.repository.country.CountryRepository
+import com.example.moviedocs.domain.repository.language.LanguageRepository
 import com.example.moviedocs.domain.repository.moviedetail.MovieDetailRepository
 import com.example.moviedocs.domain.repository.moviegenre.GenreListRepository
 import com.example.moviedocs.domain.repository.movielist.MovieListRepository
@@ -28,4 +34,19 @@ interface RepositoryModule {
   fun bindMovieDetailRepository(
     repository: MovieDetailRepositoryImpl
   ): MovieDetailRepository
+
+  @Binds
+  fun bindCompanyRepository(
+    repository: CompanyRepositoryImpl
+  ): CompanyRepository
+
+  @Binds
+  fun bindCountryRepository(
+    repository: CountryRepositoryImpl
+  ): CountryRepository
+
+  @Binds
+  fun bindLanguageRepository(
+    repository: LanguageRepositoryImpl
+  ): LanguageRepository
 }
