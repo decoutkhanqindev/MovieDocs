00e710ed6030d442c6b7f8850d49907a72f22d9b
MeansKane <phamminhkhangpy2003@gmail.com>
2024-12-08 21:46:53 +0700

    Update project

--- app/src/main/java/com/example/moviedocs/utils/dataTypeExt.kt
+++ app/src/main/java/com/example/moviedocs/utils/dataTypeExt.kt
@@ -1,30 +1,33 @@
 package com.example.moviedocs.utils
 
 import android.annotation.SuppressLint
-import com.example.moviedocs.domain.model.moviedetail.country.CountryItemModel
-import com.example.moviedocs.domain.model.moviedetail.language.LanguageItemModel
+import com.example.moviedocs.domain.model.country.CountryItemModel
+import com.example.moviedocs.domain.model.language.LanguageItemModel
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.TimeZone
 
 @SuppressLint("SimpleDateFormat")
 fun String.formatTimestamp(): String {
+  if (this.isEmpty()) {
+    return "00/00/0000"
+  }
   val inputFormat: SimpleDateFormat = SimpleDateFormat("yyyy-MM-dd")
   inputFormat.timeZone = TimeZone.getTimeZone("UTC") // Set input time zone to UTC
   val outputFormat: SimpleDateFormat = SimpleDateFormat("dd/MM/yyyy")
   val date: Date? = inputFormat.parse(this)
-  return if (date != null) outputFormat.format(date) else "Invalid Date"
+  return if (date != null) outputFormat.format(date) else "00/00/000"
 }
 
 fun String.toLanguageName(languageList: List<LanguageItemModel>): String {
-  val languageCode = this
-  val language = languageList.find { it.iso6391 == languageCode }
+  val languageCode: String = this
+  val language: LanguageItemModel? = languageList.find { it.iso6391 == languageCode }
   return language!!.englishName
 }
 
 fun String.toCountryName(countryList: List<CountryItemModel>): String {
-  val countryCode = this
-  val country = countryList.find { it.iso31661 == countryCode }
+  val countryCode: String = this
+  val country: CountryItemModel? = countryList.find { it.iso31661 == countryCode }
   return country!!.englishName
 }
 
