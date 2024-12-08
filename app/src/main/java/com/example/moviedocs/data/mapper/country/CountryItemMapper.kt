b3175ee7471cff3e5ff5498a859181dedf162dc1
MeansKane <phamminhkhangpy2003@gmail.com>
2024-12-08 21:34:42 +0700

    Handle null and empty from response for model

---
+++ app/src/main/java/com/example/moviedocs/data/mapper/country/CountryItemMapper.kt
@@ -0,0 +1,11 @@
+package com.example.moviedocs.data.mapper.country
+
+import com.example.moviedocs.data.remote.response.country.CountryItemResponse
+import com.example.moviedocs.domain.model.country.CountryItemModel
+
+fun CountryItemResponse.toCountryItemModel(): CountryItemModel =
+  CountryItemModel(
+    iso31661 = iso31661,
+    englishName = englishName?.takeIf { it.isNotEmpty() } ?: "No English name",
+    nativeName = nativeName?.takeIf { it.isNotEmpty() } ?: "No native name"
+  )
