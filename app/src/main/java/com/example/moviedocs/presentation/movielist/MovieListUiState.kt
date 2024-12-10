2d44aa8405521e90b998b16fa45760d095925241
MeansKane <phamminhkhangpy2003@gmail.com>
2024-12-10 15:16:10 +0700

    Update project

--- app/src/main/java/com/example/moviedocs/presentation/movielist/MovieListUiState.kt
+++ app/src/main/java/com/example/moviedocs/presentation/movielist/MovieListUiState.kt
@@ -5,16 +5,16 @@ import com.example.moviedocs.domain.model.movielist.MovieItemModel
 sealed interface MovieListUiState {
   data object Loading : MovieListUiState
   data class Error(val throwable: Throwable) : MovieListUiState
-  
+
   data class Success(
     val items: List<MovieItemModel>,
     val currentPage: Int,
     val totalPage: Int,
     val totalResults: Int,
   ) : MovieListUiState
-  
+
   enum class SortType {
-    TITLE_ASC, TITLE_DSC, RATING_ASC, RATING_DSC
+    TITLE_ASC, TITLE_DSC, RATING_ASC, RATING_DSC, POPULARITY_ASC, POPULARITY_DSC
   }
 }
 
