633da03c89d17d1d0a1dd9765a42849caa25ae37
MeansKane <phamminhkhangpy2003@gmail.com>
2024-12-10 16:59:04 +0700

    Handle to get data from api concurrency

--- app/src/main/java/com/example/moviedocs/presentation/moviedetail/MovieDetailViewModel.kt
+++ app/src/main/java/com/example/moviedocs/presentation/moviedetail/MovieDetailViewModel.kt
@@ -2,12 +2,19 @@ package com.example.moviedocs.presentation.moviedetail
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.moviedocs.domain.model.country.CountryItemModel
+import com.example.moviedocs.domain.model.credits.CreditListModel
+import com.example.moviedocs.domain.model.language.LanguageItemModel
 import com.example.moviedocs.domain.model.moviedetail.MovieDetailModel
+import com.example.moviedocs.domain.model.moviedetail.external.ExternalIdsModel
 import com.example.moviedocs.domain.usecase.country.GetCountryListUseCase
+import com.example.moviedocs.domain.usecase.credits.GetCreditListUseCase
 import com.example.moviedocs.domain.usecase.language.GetLanguageListUseCase
 import com.example.moviedocs.domain.usecase.moviedetail.GetExternalIdsUseCase
 import com.example.moviedocs.domain.usecase.moviedetail.GetMovieDetailUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -21,27 +28,61 @@ class MovieDetailViewModel @Inject constructor(
   private val getExternalIdsUseCase: GetExternalIdsUseCase,
   private val getLanguageListUseCase: GetLanguageListUseCase,
   private val getCountryListUseCase: GetCountryListUseCase,
+  private val getMovieCreditsUseCase: GetCreditListUseCase,
 ) : ViewModel() {
-  
+
   private val _uiState: MutableStateFlow<MovieDetailUiState> =
     MutableStateFlow(MovieDetailUiState.Loading)
   val uiState: StateFlow<MovieDetailUiState> = _uiState.asStateFlow()
-  
-  fun loadMovieDetail(movieId: Int) {
+
+  fun loadData(movieId: Int) {
     viewModelScope.launch {
       _uiState.value = MovieDetailUiState.Loading
-      
-      getMovieDetailUseCase(movieId = movieId).onSuccess { it: MovieDetailModel ->
+
+      val movieDetailDeferred: Deferred<Result<MovieDetailModel>> =
+        async { getMovieDetailUseCase(movieId) }
+      val externalIdsDeferred: Deferred<Result<ExternalIdsModel>> =
+        async { getExternalIdsUseCase(movieId) }
+      val languageListDeferred: Deferred<Result<List<LanguageItemModel>>> =
+        async { getLanguageListUseCase() }
+      val countryListDeferred: Deferred<Result<List<CountryItemModel>>> =
+        async { getCountryListUseCase() }
+      val creditListDeferred: Deferred<Result<CreditListModel>> =
+        async { getMovieCreditsUseCase(movieId) }
+
+      val movieDetailResult: Result<MovieDetailModel> = movieDetailDeferred.await()
+      val externalIdsResult: Result<ExternalIdsModel> = externalIdsDeferred.await()
+      val languageListResult: Result<List<LanguageItemModel>> = languageListDeferred.await()
+      val countryListResult: Result<List<CountryItemModel>> = countryListDeferred.await()
+      val creditListResult: Result<CreditListModel> = creditListDeferred.await()
+
+      if (movieDetailResult.isSuccess && externalIdsResult.isSuccess
+        && languageListResult.isSuccess && countryListResult.isSuccess
+        && creditListResult.isSuccess
+      ) {
         _uiState.value = MovieDetailUiState.Success(
-          movieDetail = it,
-          externalIds = getExternalIdsUseCase(movieId = movieId).getOrThrow(),
-          languageList = getLanguageListUseCase().getOrThrow(),
-          countryList = getCountryListUseCase().getOrThrow(),
+          movieDetail = movieDetailResult.getOrNull()!!,
+          externalIds = externalIdsResult.getOrNull()!!,
+          languageList = languageListResult.getOrNull()!!,
+          countryList = countryListResult.getOrNull()!!,
+          castList = creditListResult.getOrNull()?.cast ?: emptyList(),
+          creditList = creditListResult.getOrNull()?.crew ?: emptyList(),
         )
-      }.onFailure { it: Throwable ->
-        _uiState.value = MovieDetailUiState.Error(throwable = it)
-        Timber.tag(this.javaClass.simpleName).e("getMovieDetail: $it")
+      } else {
+        val error = listOf(
+          movieDetailResult, externalIdsResult, languageListResult, countryListResult,
+          creditListResult
+        ).first { it.isFailure }
+        _uiState.value = MovieDetailUiState.Error(error.exceptionOrNull() ?: Throwable())
+        Timber.tag(this.javaClass.simpleName).e("loadData ${error.exceptionOrNull()}")
       }
     }
   }
+
+//  private fun updateUiState(update: (MovieDetailUiState.Success) -> MovieDetailUiState.Success) {
+//    val currentState: MovieDetailUiState = _uiState.value
+//    if (currentState is MovieDetailUiState.Success) {
+//      _uiState.value = update(currentState)
+//    }
+//  }
 }
