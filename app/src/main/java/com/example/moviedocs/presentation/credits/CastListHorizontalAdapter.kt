b3b47ebc55babe9afa762e963ac9a8114e803894
MeansKane <phamminhkhangpy2003@gmail.com>
2024-12-10 17:00:22 +0700

    Add crew and cast list horizontal adapter

---
+++ app/src/main/java/com/example/moviedocs/presentation/credits/CastListHorizontalAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.moviedocs.presentation.credits
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.moviedocs.databinding.CreditItemHorizontalViewHolderBinding
+import com.example.moviedocs.domain.model.credits.CastItemModel
+import com.example.moviedocs.presentation.base.BaseListAdapter
+import com.example.moviedocs.utils.loadImgFromUrl
+
+class CastListHorizontalAdapter :
+  BaseListAdapter<CastItemModel, CreditItemHorizontalViewHolderBinding>(
+    CastItemDiffCallBack
+  ) {
+
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder =
+    VH(
+      CreditItemHorizontalViewHolderBinding.inflate(
+        LayoutInflater.from(parent.context), parent, false
+      )
+    )
+
+  private inner class VH(
+    binding: CreditItemHorizontalViewHolderBinding
+  ) : BaseViewHolder(binding) {
+
+    init {
+      binding.root.setOnClickListener {
+        val position: Int = bindingAdapterPosition
+        if (position != RecyclerView.NO_POSITION)
+          onItemClickListener?.invoke(getItem(position))
+      }
+    }
+
+    override fun bind(item: CastItemModel) {
+      binding.apply {
+        creditItemImg.loadImgFromUrl(item.profilePath)
+        creditItemName.text = item.name
+      }
+    }
+  }
+}
--- app/src/main/java/com/example/moviedocs/presentation/moviedetail/credits/MovieDetailCreditsFragment.kt
+++ app/src/main/java/com/example/moviedocs/presentation/moviedetail/credits/MovieDetailCreditsFragment.kt
@@ -2,8 +2,18 @@ package com.example.moviedocs.presentation.moviedetail.credits
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.moviedocs.databinding.FragmentMovieDetailCreditsBinding
 import com.example.moviedocs.presentation.base.BaseFragment
+import com.example.moviedocs.presentation.credits.CastListHorizontalAdapter
+import com.example.moviedocs.presentation.credits.CrewListHorizontalAdapter
+import com.example.moviedocs.presentation.moviedetail.MovieDetailUiState
+import com.example.moviedocs.presentation.moviedetail.MovieDetailViewModel
+import com.example.moviedocs.utils.invisible
+import com.example.moviedocs.utils.launchAndRepeatStarted
+import com.example.moviedocs.utils.setUpRecyclerView
+import com.example.moviedocs.utils.visible
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -14,7 +24,76 @@ class MovieDetailCreditsFragment : BaseFragment<FragmentMovieDetailCreditsBindin
     fun newInstance(): MovieDetailCreditsFragment = MovieDetailCreditsFragment()
   }
 
+  private val viewModel: MovieDetailViewModel by activityViewModels()
+
+  private val castListHorizontalAdapter: CastListHorizontalAdapter by lazy(LazyThreadSafetyMode.NONE) {
+    CastListHorizontalAdapter()
+  }
+
+  private val crewListHorizontalAdapter: CrewListHorizontalAdapter by lazy(LazyThreadSafetyMode.NONE) {
+    CrewListHorizontalAdapter()
+  }
+
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
+
+    setUpRecyclerView(
+      mRecyclerView = binding.castRecycleView,
+      mLayoutManager = LinearLayoutManager(
+        requireContext(), LinearLayoutManager.HORIZONTAL, false
+      ),
+      mAdapter = castListHorizontalAdapter,
+    )
+    setUpRecyclerView(
+      mRecyclerView = binding.crewRecycleView,
+      mLayoutManager = LinearLayoutManager(
+        requireContext(), LinearLayoutManager.HORIZONTAL, false
+      ),
+      mAdapter = crewListHorizontalAdapter,
+    )
+    bindViewModel()
+  }
+
+  private fun bindViewModel() {
+    launchAndRepeatStarted({ viewModel.uiState.collect(::renderUi) })
+  }
+
+  private fun renderUi(state: MovieDetailUiState) {
+    when (state) {
+      MovieDetailUiState.Loading -> {
+        binding.apply {
+          castLayout.invisible()
+          crewLayout.invisible()
+        }
+        castListHorizontalAdapter.submitList(emptyList())
+        crewListHorizontalAdapter.submitList(emptyList())
+      }
+
+      is MovieDetailUiState.Success -> {
+        binding.apply {
+          castLayout.visible()
+          crewLayout.visible()
+        }
+        castListHorizontalAdapter.submitList(state.castList)
+        crewListHorizontalAdapter.submitList(state.creditList)
+      }
+
+      is MovieDetailUiState.Error -> {
+        binding.apply {
+          castLayout.invisible()
+          crewLayout.invisible()
+        }
+        castListHorizontalAdapter.submitList(emptyList())
+        crewListHorizontalAdapter.submitList(emptyList())
+      }
+    }
+  }
+
+  override fun onDestroyView() {
+    binding.apply {
+      castRecycleView.adapter = null
+      crewRecycleView.adapter = null
+    }
+    super.onDestroyView()
   }
 }
