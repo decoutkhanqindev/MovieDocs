2d00c9d9f613ad6cb77e304d52b2b4000204d2f6
MeansKane <phamminhkhangpy2003@gmail.com>
2024-12-10 15:14:52 +0700

    Add credits repository in app repository module

--- app/src/main/java/com/example/moviedocs/di/RepositoryModule.kt
+++ app/src/main/java/com/example/moviedocs/di/RepositoryModule.kt
@@ -2,12 +2,14 @@ package com.example.moviedocs.di
 
 import com.example.moviedocs.data.repository.company.CompanyRepositoryImpl
 import com.example.moviedocs.data.repository.country.CountryRepositoryImpl
+import com.example.moviedocs.data.repository.credits.CreditsRepositoryImpl
 import com.example.moviedocs.data.repository.language.LanguageRepositoryImpl
 import com.example.moviedocs.data.repository.moviedetail.MovieDetailRepositoryImpl
 import com.example.moviedocs.data.repository.moviegenre.GenreListRepositoryImpl
 import com.example.moviedocs.data.repository.movielist.MovieListRepositoryImpl
 import com.example.moviedocs.domain.repository.company.CompanyRepository
 import com.example.moviedocs.domain.repository.country.CountryRepository
+import com.example.moviedocs.domain.repository.credits.CreditsRepository
 import com.example.moviedocs.domain.repository.language.LanguageRepository
 import com.example.moviedocs.domain.repository.moviedetail.MovieDetailRepository
 import com.example.moviedocs.domain.repository.moviegenre.GenreListRepository
@@ -24,12 +26,12 @@ interface RepositoryModule {
   fun bindMovieListRepository(
     repository: MovieListRepositoryImpl
   ): MovieListRepository
-  
+
   @Binds
   fun bindGenreListRepository(
     repository: GenreListRepositoryImpl
   ): GenreListRepository
-  
+
   @Binds
   fun bindMovieDetailRepository(
     repository: MovieDetailRepositoryImpl
@@ -49,4 +51,9 @@ interface RepositoryModule {
   fun bindLanguageRepository(
     repository: LanguageRepositoryImpl
   ): LanguageRepository
+
+  @Binds
+  fun bindCreditsRepository(
+    repository: CreditsRepositoryImpl
+  ): CreditsRepository
 }
