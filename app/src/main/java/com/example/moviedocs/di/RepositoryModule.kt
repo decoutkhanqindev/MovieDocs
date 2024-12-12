2dfa1c27b4a3b3f39d56f9207ca2c04213aac6b8
MeansKane <phamminhkhangpy2003@gmail.com>
2024-12-13 00:21:34 +0700

    Add func to bind external ids repository in repository module

--- app/src/main/java/com/example/moviedocs/di/RepositoryModule.kt
+++ app/src/main/java/com/example/moviedocs/di/RepositoryModule.kt
@@ -3,6 +3,7 @@ package com.example.moviedocs.di
 import com.example.moviedocs.data.repository.company.CompanyRepositoryImpl
 import com.example.moviedocs.data.repository.country.CountryRepositoryImpl
 import com.example.moviedocs.data.repository.credits.CreditsRepositoryImpl
+import com.example.moviedocs.data.repository.externalId.ExternalIdRepositoryImpl
 import com.example.moviedocs.data.repository.genre.GenreRepositoryImpl
 import com.example.moviedocs.data.repository.language.LanguageRepositoryImpl
 import com.example.moviedocs.data.repository.moviedetail.MovieDetailRepositoryImpl
@@ -10,6 +11,7 @@ import com.example.moviedocs.data.repository.movielist.MovieListRepositoryImpl
 import com.example.moviedocs.domain.repository.company.CompanyRepository
 import com.example.moviedocs.domain.repository.country.CountryRepository
 import com.example.moviedocs.domain.repository.credits.CreditsRepository
+import com.example.moviedocs.domain.repository.externalId.ExternalIdRepository
 import com.example.moviedocs.domain.repository.genre.GenreRepository
 import com.example.moviedocs.domain.repository.language.LanguageRepository
 import com.example.moviedocs.domain.repository.moviedetail.MovieDetailRepository
@@ -56,4 +58,9 @@ interface RepositoryModule {
   fun bindCreditsRepository(
     repository: CreditsRepositoryImpl
   ): CreditsRepository
+
+  @Binds
+  fun bindExternalIdRepository(
+    repository: ExternalIdRepositoryImpl
+  ): ExternalIdRepository
 }
