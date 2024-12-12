a3007db15641e74b3234595160992a0765e64300
MeansKane <phamminhkhangpy2003@gmail.com>
2024-12-13 00:20:07 +0700

    Add external ids repository

---
+++ app/src/main/java/com/example/moviedocs/data/repository/externalId/ExternalIdRepositoryImpl.kt
@@ -0,0 +1,28 @@
+package com.example.moviedocs.data.repository.externalId
+
+import com.example.moviedocs.data.mapper.externalId.toExternalIdsModel
+import com.example.moviedocs.data.remote.ApiService
+import com.example.moviedocs.di.AppDispatcher
+import com.example.moviedocs.di.DispatcherType
+import com.example.moviedocs.domain.externalId.ExternalIdsModel
+import com.example.moviedocs.domain.repository.externalId.ExternalIdRepository
+import com.example.moviedocs.utils.runSuspendCatching
+import kotlinx.coroutines.CoroutineDispatcher
+import javax.inject.Inject
+
+class ExternalIdRepositoryImpl @Inject constructor(
+  private val apiService: ApiService,
+  @AppDispatcher(DispatcherType.IO)
+  private val ioDispatcher: CoroutineDispatcher,
+) : ExternalIdRepository {
+
+  override suspend fun getMovieDetailExternalIds(movieId: Int): Result<ExternalIdsModel> =
+    runSuspendCatching(ioDispatcher) {
+      apiService.getMovieDetailExternalIds(movieId = movieId).toExternalIdsModel()
+    }
+
+  override suspend fun getPersonDetailExternalIds(personId: Int): Result<ExternalIdsModel> =
+    runSuspendCatching(ioDispatcher) {
+      apiService.getPersonDetailExternalIds(personId = personId).toExternalIdsModel()
+    }
+}
