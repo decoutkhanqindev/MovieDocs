e53f1723dd475eafda128b7a48943deffc86f7b5
MeansKane <phamminhkhangpy2003@gmail.com>
2024-12-13 00:21:52 +0700

    Add person detail response

---
+++ app/src/main/java/com/example/moviedocs/data/remote/response/person/PersonDetailResponse.kt
@@ -0,0 +1,36 @@
+package com.example.moviedocs.data.remote.response.person
+
+import androidx.annotation.Keep
+import com.squareup.moshi.Json
+
+@Keep
+data class PersonDetailResponse(
+  @Json(name = "adult")
+  val adult: Boolean?,
+  @Json(name = "also_known_as")
+  val alsoKnownAs: List<String?>?,
+  @Json(name = "biography")
+  val biography: String?,
+  @Json(name = "birthday")
+  val birthday: String?,
+  @Json(name = "deathday")
+  val deathday: Any?,
+  @Json(name = "gender")
+  val gender: Int?,
+  @Json(name = "homepage")
+  val homepage: Any?,
+  @Json(name = "id")
+  val id: Int,
+  @Json(name = "imdb_id")
+  val imdbId: String,
+  @Json(name = "known_for_department")
+  val knownForDepartment: String?,
+  @Json(name = "name")
+  val name: String?,
+  @Json(name = "place_of_birth")
+  val placeOfBirth: String?,
+  @Json(name = "popularity")
+  val popularity: Double?,
+  @Json(name = "profile_path")
+  val profilePath: String?
+)
