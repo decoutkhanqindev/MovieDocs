d3d82b75a5b0e34a0b24300b1794fcfcac1a8da9
MeansKane <phamminhkhangpy2003@gmail.com>
2024-12-14 22:05:29 +0700

    Add person movie credits response

---
+++ app/src/main/java/com/example/moviedocs/data/remote/response/person/PersonMovieCreditListResponse.kt
@@ -0,0 +1,13 @@
+package com.example.moviedocs.data.remote.response.person
+
+import com.example.moviedocs.data.remote.response.movielist.MovieItemResponse
+import com.squareup.moshi.Json
+
+data class PersonMovieCreditListResponse(
+  @Json(name = "cast")
+  val cast: List<MovieItemResponse>,
+  @Json(name = "crew")
+  val crew: List<MovieItemResponse>,
+  @Json(name = "id")
+  val id: Int // movie id
+)
