210990903ba391cace2ece14b4a0c8254d842b01
MeansKane <phamminhkhangpy2003@gmail.com>
2024-12-14 22:04:57 +0700

    Add get person detail use case

---
+++ app/src/main/java/com/example/moviedocs/domain/usecase/person/GetPersonDetailUseCase.kt
@@ -0,0 +1,12 @@
+package com.example.moviedocs.domain.usecase.person
+
+import com.example.moviedocs.domain.person.PersonDetailModel
+import com.example.moviedocs.domain.repository.person.PersonRepository
+import javax.inject.Inject
+
+class GetPersonDetailUseCase @Inject constructor(
+  private val repository: PersonRepository
+) {
+  suspend operator fun invoke(personId: Int): Result<PersonDetailModel> =
+    repository.getPersonDetail(personId)
+}
