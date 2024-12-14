63faeb48d2295738663976e4474684bd26aaf79e
MeansKane <phamminhkhangpy2003@gmail.com>
2024-12-14 22:05:41 +0700

    Add person movie credits mapper func

---
+++ app/src/main/java/com/example/moviedocs/data/mapper/person/PersonMovieCreditListMapper.kt
@@ -0,0 +1,12 @@
+package com.example.moviedocs.data.mapper.person
+
+import com.example.moviedocs.data.mapper.movielist.toMovieItemModel
+import com.example.moviedocs.data.remote.response.person.PersonMovieCreditListResponse
+import com.example.moviedocs.domain.person.PersonMovieCreditListModel
+
+fun PersonMovieCreditListResponse.toPersonMovieCreditListModel(): PersonMovieCreditListModel =
+  PersonMovieCreditListModel(
+    cast = cast.map { it.toMovieItemModel() },
+    crew = crew.map { it.toMovieItemModel() },
+    id = id
+  )
