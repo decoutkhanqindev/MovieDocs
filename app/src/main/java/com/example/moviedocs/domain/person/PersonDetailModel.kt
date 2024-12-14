2a79a5d782c3042e404f8f8a3a2c94967a8d867f
MeansKane <phamminhkhangpy2003@gmail.com>
2024-12-14 22:04:30 +0700

    Add person detail model

---
+++ app/src/main/java/com/example/moviedocs/domain/person/PersonDetailModel.kt
@@ -0,0 +1,16 @@
+package com.example.moviedocs.domain.person
+
+data class PersonDetailModel(
+  val alsoKnownAs: List<String>,
+  val biography: String,
+  val birthday: String,
+  val deathday: String,
+  val gender: String,
+  val homepage: String,
+  val id: Int,
+  val knownForDepartment: String,
+  val name: String,
+  val placeOfBirth: String,
+  val popularity: Double,
+  val profilePath: String
+)
