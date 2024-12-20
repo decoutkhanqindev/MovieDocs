6b13ad01d702a253202847116a6a361c53c7fe2b
MeansKane <phamminhkhangpy2003@gmail.com>
2024-12-20 16:43:26 +0700

    Add slider repository

--- app/src/main/java/com/example/moviedocs/data/repository/slider/SliderRepositoryImpl.kt
+++ app/src/main/java/com/example/moviedocs/data/repository/slider/SliderRepositoryImpl.kt
@@ -3,7 +3,7 @@ package com.example.moviedocs.data.repository.slider
 import com.example.moviedocs.R
 import com.example.moviedocs.di.AppDispatcher
 import com.example.moviedocs.di.DispatcherType
-import com.example.moviedocs.domain.model.slider.SliderImageModel
+import com.example.moviedocs.domain.model.slider.SliderImageItemModel
 import com.example.moviedocs.domain.repository.slider.SliderRepository
 import com.example.moviedocs.utils.runSuspendCatching
 import jakarta.inject.Inject
@@ -13,12 +13,12 @@ class SliderRepositoryImpl @Inject constructor(
   @AppDispatcher(DispatcherType.IO)
   private val ioDispatcher: CoroutineDispatcher
 ) : SliderRepository {
-  override suspend fun getSliderImage(): Result<List<SliderImageModel>> =
+  override suspend fun getSliderImage(): Result<List<SliderImageItemModel>> =
     runSuspendCatching(ioDispatcher) {
       listOf(
-        SliderImageModel(1, R.drawable.wide),
-        SliderImageModel(2, R.drawable.wide1),
-        SliderImageModel(3, R.drawable.wide3)
+        SliderImageItemModel(1, R.drawable.wide),
+        SliderImageItemModel(2, R.drawable.wide1),
+        SliderImageItemModel(3, R.drawable.wide3)
       )
     }
 }
--- app/src/main/java/com/example/moviedocs/domain/repository/slider/SliderRepository.kt
+++ app/src/main/java/com/example/moviedocs/domain/repository/slider/SliderRepository.kt
@@ -1,7 +1,7 @@
 package com.example.moviedocs.domain.repository.slider
 
-import com.example.moviedocs.domain.model.slider.SliderImageModel
+import com.example.moviedocs.domain.model.slider.SliderImageItemModel
 
 interface SliderRepository {
-  suspend fun getSliderImage(): Result<List<SliderImageModel>>
+  suspend fun getSliderImage(): Result<List<SliderImageItemModel>>
 }
