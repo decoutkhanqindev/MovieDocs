024d46253d1a07266554b719cce988fe707924ba
MeansKane <phamminhkhangpy2003@gmail.com>
2024-12-20 16:43:52 +0700

    Handle get slider data

--- app/src/main/java/com/example/moviedocs/presentation/home/HomeUiState.kt
+++ app/src/main/java/com/example/moviedocs/presentation/home/HomeUiState.kt
@@ -2,11 +2,13 @@ package com.example.moviedocs.presentation.home
 
 import com.example.moviedocs.domain.model.genre.GenreItemModel
 import com.example.moviedocs.domain.model.movielist.MovieItemModel
+import com.example.moviedocs.domain.model.slider.SliderImageItemModel
 
 sealed interface HomeUiState {
   data object Loading : HomeUiState
   data class Error(val throwable: Throwable) : HomeUiState
   data class Success(
+    val sliderList: List<SliderImageItemModel> = emptyList(),
     val genreList: List<GenreItemModel> = emptyList(),
     val nowPlaying: List<MovieItemModel> = emptyList(),
     val popular: List<MovieItemModel> = emptyList(),
