e7f7ff5df99f4e0fdee367478819433c2afab802
MeansKane <phamminhkhangpy2003@gmail.com>
2024-12-20 13:10:28 +0700

    Handel visibility movie item character and department for cast and crew fragments

--- app/src/main/java/com/example/moviedocs/presentation/movielist/year/MovieListItemByYearAdapter.kt
+++ app/src/main/java/com/example/moviedocs/presentation/movielist/year/MovieListItemByYearAdapter.kt
@@ -8,6 +8,8 @@ import com.example.moviedocs.databinding.MovieListItemByYearViewHolderBinding
 import com.example.moviedocs.domain.model.movielist.MovieItemModel
 import com.example.moviedocs.presentation.base.BaseListAdapter
 import com.example.moviedocs.presentation.movielist.MovieItemModelDiffCallBack
+import com.example.moviedocs.utils.gone
+import com.example.moviedocs.utils.visible
 
 class MovieListItemByYearAdapter :
   BaseListAdapter<MovieItemModel, MovieListItemByYearViewHolderBinding>(
@@ -39,7 +41,15 @@ class MovieListItemByYearAdapter :
     override fun bind(item: MovieItemModel) {
       binding.apply {
         movieItemName.text = item.title
-        movieItemCharacter.text = "as ${item.character}"
+        if (item.character == "Unknown character") {
+          movieItemCharacter.gone()
+          movieItemDepartment.visible()
+          movieItemDepartment.text = "as ${item.department}"
+        } else {
+          movieItemCharacter.visible()
+          movieItemDepartment.gone()
+          movieItemCharacter.text = "as ${item.character}"
+        }
       }
     }
   }
