2ccb58e33f35d2ee2c25a4cdd01ed05126dbea6a
MeansKane <phamminhkhangpy2003@gmail.com>
2024-12-21 00:40:33 +0700

    Add base view holder

--- app/src/main/java/com/example/moviedocs/presentation/base/BaseListAdapter.kt
+++ app/src/main/java/com/example/moviedocs/presentation/base/BaseListAdapter.kt
@@ -2,31 +2,22 @@ package com.example.moviedocs.presentation.base
 
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
 
 abstract class BaseListAdapter<T : Any, VB : ViewBinding>(
   diffCallBack: DiffUtil.ItemCallback<T>,
-) : ListAdapter<T, BaseListAdapter<T, VB>.BaseViewHolder>(
-  diffCallBack
-) {
+) : ListAdapter<T, BaseViewHolder<T, VB>>(diffCallBack) {
 
   // init in child adapter
   internal var onItemClickListener: ((T) -> Unit)? = null
 
-  override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
+  override fun onBindViewHolder(holder: BaseViewHolder<T, VB>, position: Int) {
     holder.bind(getItem(position))
   }
 
   // activity or fragment call
   fun setOnItemClickListener(listener: (T) -> Unit) {
     onItemClickListener = listener
-  }
-
-  abstract inner class BaseViewHolder(
-    protected val binding: VB,
-  ) : RecyclerView.ViewHolder(binding.root) {
-    abstract fun bind(item: T)
   }
 }
 
---
+++ app/src/main/java/com/example/moviedocs/presentation/base/BaseViewHolder.kt
@@ -0,0 +1,10 @@
+package com.example.moviedocs.presentation.base
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseViewHolder<T : Any, VB : ViewBinding>(
+  protected val binding: VB,
+) : RecyclerView.ViewHolder(binding.root) {
+  internal abstract fun bind(item: T)
+}
