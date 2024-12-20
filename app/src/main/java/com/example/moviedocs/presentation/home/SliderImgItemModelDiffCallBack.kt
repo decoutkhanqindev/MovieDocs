5a2188e05677e38b1b12c5aedd98812914d82368
MeansKane <phamminhkhangpy2003@gmail.com>
2024-12-20 16:43:18 +0700

    Add slider image item model diff callback

---
+++ app/src/main/java/com/example/moviedocs/presentation/home/SliderImgItemModelDiffCallBack.kt
@@ -0,0 +1,14 @@
+package com.example.moviedocs.presentation.home
+
+import androidx.recyclerview.widget.DiffUtil
+import com.example.moviedocs.domain.model.slider.SliderImageItemModel
+
+object SliderImgItemModelDiffCallBack : DiffUtil.ItemCallback<SliderImageItemModel>() {
+  override fun areContentsTheSame(
+    oldItem: SliderImageItemModel, newItem: SliderImageItemModel,
+  ): Boolean = oldItem == newItem
+
+  override fun areItemsTheSame(
+    oldItem: SliderImageItemModel, newItem: SliderImageItemModel,
+  ): Boolean = oldItem.id == newItem.id
+}
