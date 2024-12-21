b994bae3fc8ff84355022c23464cc01bcde60aa2
MeansKane <phamminhkhangpy2003@gmail.com>
2024-12-22 00:03:03 +0700

    Add movie detail image item response

---
+++ app/src/main/java/com/example/moviedocs/data/remote/response/moviedetails/image/ImageItemResponse.kt
@@ -0,0 +1,21 @@
+package com.example.moviedocs.data.remote.response.moviedetails.image
+
+
+import com.squareup.moshi.Json
+
+data class ImageItemResponse(
+  @Json(name = "aspect_ratio")
+  val aspectRatio: Double, // 1.778
+  @Json(name = "file_path")
+  val filePath: String, // /8PwntYBvxnaJq6R57UiXGNFsluN.jpg
+  @Json(name = "height")
+  val height: Int, // 1080
+  @Json(name = "iso_639_1")
+  val iso6391: String?, // pt
+  @Json(name = "vote_average")
+  val voteAverage: Double, // 8.034
+  @Json(name = "vote_count")
+  val voteCount: Int, // 5
+  @Json(name = "width")
+  val width: Int // 1920
+)
