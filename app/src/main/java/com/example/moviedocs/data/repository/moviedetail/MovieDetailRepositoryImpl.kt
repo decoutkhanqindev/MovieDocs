5f5984caede4885b47202527a4fd995881fceb1c
MeansKane <phamminhkhangpy2003@gmail.com>
2024-12-26 21:00:37 +0700

    Add get movie images in movie detail repository

--- app/src/main/java/com/example/moviedocs/data/repository/moviedetail/MovieDetailRepositoryImpl.kt
+++ app/src/main/java/com/example/moviedocs/data/repository/moviedetail/MovieDetailRepositoryImpl.kt
@@ -1,10 +1,12 @@
 package com.example.moviedocs.data.repository.moviedetail
 
+import com.example.moviedocs.data.mapper.moviedetail.image.toImageListModel
 import com.example.moviedocs.data.mapper.moviedetail.toMovieDetailModel
 import com.example.moviedocs.data.remote.ApiService
 import com.example.moviedocs.di.AppDispatcher
 import com.example.moviedocs.di.DispatcherType
 import com.example.moviedocs.domain.model.moviedetail.MovieDetailModel
+import com.example.moviedocs.domain.model.moviedetail.image.ImageListModel
 import com.example.moviedocs.domain.repository.moviedetail.MovieDetailRepository
 import com.example.moviedocs.utils.runSuspendCatching
 import kotlinx.coroutines.CoroutineDispatcher
@@ -19,5 +21,10 @@ class MovieDetailRepositoryImpl @Inject constructor(
   override suspend fun getMovieDetail(movieId: Int): Result<MovieDetailModel> =
     runSuspendCatching(ioDispatcher) {
       apiService.getMovieDetail(movieId = movieId).toMovieDetailModel()
+    }
+
+  override suspend fun getMovieImages(movieId: Int): Result<ImageListModel> =
+    runSuspendCatching(ioDispatcher) {
+      apiService.getMovieImages(movieId).toImageListModel()
     }
 }
--- app/src/main/java/com/example/moviedocs/domain/repository/moviedetail/MovieDetailRepository.kt
+++ app/src/main/java/com/example/moviedocs/domain/repository/moviedetail/MovieDetailRepository.kt
@@ -1,7 +1,9 @@
 package com.example.moviedocs.domain.repository.moviedetail
 
 import com.example.moviedocs.domain.model.moviedetail.MovieDetailModel
+import com.example.moviedocs.domain.model.moviedetail.image.ImageListModel
 
 interface MovieDetailRepository {
   suspend fun getMovieDetail(movieId: Int): Result<MovieDetailModel>
+  suspend fun getMovieImages(movieId: Int): Result<ImageListModel>
 }
