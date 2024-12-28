1b7a3c706b15905ab4d7467c2e09db90a5cb5536
MeansKane <phamminhkhangpy2003@gmail.com>
2024-12-28 12:55:53 +0700

    Add media item model diff callback

---
+++ app/src/main/java/com/example/moviedocs/presentation/media/MediaItemModelDiffCallback.kt
@@ -0,0 +1,16 @@
+package com.example.moviedocs.presentation.media
+
+import androidx.recyclerview.widget.DiffUtil
+import com.example.moviedocs.domain.model.moviedetail.image.MediaItemModel
+
+object MediaItemModelDiffCallback : DiffUtil.ItemCallback<MediaItemModel>() {
+  override fun areItemsTheSame(
+    oldItem: MediaItemModel,
+    newItem: MediaItemModel
+  ): Boolean = oldItem.filePath == newItem.filePath
+
+  override fun areContentsTheSame(
+    oldItem: MediaItemModel,
+    newItem: MediaItemModel
+  ): Boolean = oldItem == newItem
+}
