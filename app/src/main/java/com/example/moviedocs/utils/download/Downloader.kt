9f0ac37786eab82272af37e5a887de73b71d3974
MeansKane <phamminhkhangpy2003@gmail.com>
2024-12-30 21:38:19 +0700

    Add downloader impl to handle download img

--- app/src/main/java/com/example/moviedocs/utils/download/Downloader.kt
+++ app/src/main/java/com/example/moviedocs/utils/download/Downloader.kt
@@ -1,5 +1,5 @@
 package com.example.moviedocs.utils.download
 
 interface Downloader {
-  fun downloadFile(url: String): Long
+  fun downloadFile(url: String, filename: String, mimeType: String): Long
 }
---
+++ app/src/main/java/com/example/moviedocs/utils/download/DownloaderImpl.kt
@@ -0,0 +1,22 @@
+package com.example.moviedocs.utils.download
+
+import android.app.DownloadManager
+import android.content.Context
+import android.os.Environment
+import androidx.core.net.toUri
+
+class DownloaderImpl(context: Context) : Downloader {
+
+  private val downloadManager: DownloadManager =
+    context.getSystemService(DownloadManager::class.java)
+
+  override fun downloadFile(url: String, filename: String, mimeType: String): Long {
+    val request: DownloadManager.Request = DownloadManager.Request(url.toUri())
+      .setTitle("Download $filename")
+      .setMimeType(mimeType)
+      .setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI)
+      .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, filename)
+      .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+    return downloadManager.enqueue(request)
+  }
+}
